b460d9d8cb75b681551fffe5ef665abd3e6da9e3
nvijayku <user@15-745-s14.(none)>
2014-04-28 21:08:15 -0400

    Added test3.bc

--- llvm/bench.sh
+++ llvm/bench.sh
@@ -4,3 +4,6 @@ llvm-dis test-m2r.bc
 clang -O0 -emit-llvm -c test2.c
 opt -mem2reg test2.bc -o test2-m2r.bc
 llvm-dis test2-m2r.bc
+clang -O0 -emit-llvm -c test3.c
+opt -mem2reg test3.bc -o test3-m2r.bc
+llvm-dis test3-m2r.bc
---
+++ llvm/test3.c
@@ -0,0 +1,39 @@
+#include <malloc.h>
+
+struct test {int crap; char shit;};
+
+int main () {
+
+  struct test a[10];
+
+  struct test test1;
+  int c[10];
+  int *d;
+  
+  struct test* b;
+  test1.crap = 4;
+
+  int i;
+  for (i = 0; i < 10; i++) {
+	a[i].crap=i;
+	a[i].shit=i;
+	c[i] = i;
+  }
+
+  b = a + test1.crap;
+  d = c;
+
+  b->crap = 20;
+
+  int sum = 0;
+
+  for (i = 0; i < 10; i++) {
+	sum += a[i].crap;
+	sum += a[i].shit;
+	sum += d[i];
+	sum += c[i];
+  }
+
+  return sum;
+
+}
